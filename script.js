da15dade155051fdca3152d2555ffcc129de7560
kamillysantosramos-ship-it <kamilly.santos.ramos@escola.pr.gov.br>
2026-08-21 11:07:18 -0300

    ok

--- script.js
+++ script.js
@@ -1,3 +1,31 @@
 document.addEventListener("DOMContentLoadea",()=>{
-const likeBtn= document.querySelector(".left-actions.action-Btn")
+    //selecione o botão de curtida e o icone de coração
+const likeBtn= document.querySelector(".left-actions.action-Btn:first-child");
+if (!likeBtn) return;
+
+const likeSvg = likeBtn.querySelector("svg");
+
+//contador
+let textNode = Array.from(likeBtn.childNodes). find(
+    (node) => node.nodeType === Node. TEXT_NODE && node.textContent.trim()!==""
+);
+
+//Zerando o contador inicial.
+let count = 0;
+
+//Atualiza
+if(textNode){
+    textNode.textContent = `0`;
+}
 })
+
+
+
+
+//coração 
+function applyLikesdStyle(){
+    likeSvg.style.fill= #0000FF
+    likeSvg.style.stroke= #0000FF
+    likeBtn.style.color= #0000FF
+
+    //efeito visual de clique (pop/bounce)
